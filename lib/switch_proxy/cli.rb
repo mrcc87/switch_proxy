4b00ec50db2fa24b02c37237724edcf72a166b05
Mircea Samuila <mrcc87@gmail.com>
2017-01-27 23:41:01 +0100

    changed on color to black

--- lib/switch_proxy/cli.rb
+++ lib/switch_proxy/cli.rb
@@ -5,7 +5,7 @@ module SwitchProxy
     def toggle
       if options[:on]
         ProxySetter.new.add_proxy
-        puts set_color "proxy on", :green, :on_white
+        puts set_color "proxy on", :black, :on_white
       else
         ProxySetter.new.remove_proxy
         puts set_color "proxy off", :red, :on_white
